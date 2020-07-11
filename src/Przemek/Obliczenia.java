9154a0df6bd31b9593255ed392280797e78a5565
tavirek <przemyslawdopart@gmail.com>
2020-07-11 20:01:36 +0200

    Zrobione zadanie

--- src/Przemek/Obliczenia.java
+++ src/Przemek/Obliczenia.java
@@ -2,15 +2,34 @@ package Przemek;
 
 import java.util.Scanner;
 
+
 public class Obliczenia {
     public static void main(String[] args) {
-        int a, b, c;
-        Scanner obliczacz = new Scanner(System.in);
+        int a, b, c, d;
+        float e;
+        Scanner liczacz = new Scanner(System.in);
+
+
         System.out.println("Podaj liczbę a");
-         a = obliczacz.nextInt();
+        a = liczacz.nextInt();
         System.out.println("Podaj liczbę b");
-         b = obliczacz.nextInt();
-        System.out.println("suma:" a + b);
+        b = liczacz.nextInt();
+        c = a + b;
+        System.out.println("suma: " + c);
+        d = (int)Math.pow(c,2);
+        System.out.println("Potęga: " + d );
+        e = (float)Math.sqrt(c);
+        System.out.println("Pierwiastek: " + e);
+        if (a > b) {
+            System.out.println("Większa jest liczba: a" + a );
+        }
+        if (a < b) {
+            System.out.println("Większa jest liczba: b" + b);
+        }
+        if (a == b) {
+            System.out.println("Liczby takie same");
+        }
+
 
     }
 }
