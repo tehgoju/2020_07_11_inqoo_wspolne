5bccd309e44a0ca38b3ab0da62ed20bccc8f1f0b
Olaf Goj <olaf@goj.email>
2020-07-11 12:41:32 +0200

    Zmienilem powitanie

--- src/com/inqoo/basics/HelloWorld.java
+++ src/com/inqoo/basics/HelloWorld.java
@@ -3,6 +3,6 @@ package com.inqoo.basics;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Witaj!");
+        System.out.println("Czesc!");
     }
 }
