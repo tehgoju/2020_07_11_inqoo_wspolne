a91ca946af40680c43c4ab25d5eabf4cffcdc2cb
Olaf Goj <olaf@goj.email>
2020-07-11 12:36:27 +0200

    Dodano komentarz, usunieto sout

--- src/com/inqoo/basics/Comments.java
+++ src/com/inqoo/basics/Comments.java
@@ -6,10 +6,10 @@ public class Comments {
         System.out.println("Witaj na "); //Powitanie
         System.out.println("kursie programowania");
 
+        // jakis komentarz
         /* Na koniec emotka,
           bo powaga zabija ;)
          */
         // ponizsza linijka wypisze emotikone
-        System.out.println(":)");
     }
 }
