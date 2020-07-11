fc330fb1813536a69779ea6becbe9f4462e82fda
tavirek <przemyslawdopart@gmail.com>
2020-07-11 15:32:08 +0200

    Obliczenie

---
+++ src/Przemek/Obliczenia.java
@@ -0,0 +1,16 @@
+package Przemek;
+
+import java.util.Scanner;
+
+public class Obliczenia {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner obliczacz = new Scanner(System.in);
+        System.out.println("Podaj liczbę a");
+         a = obliczacz.nextInt();
+        System.out.println("Podaj liczbę b");
+         b = obliczacz.nextInt();
+        System.out.println("suma:" a + b);
+
+    }
+}
