353817db9f4deb7f3df19c2ec901307e203aa86d
tavirek <przemyslawdopart@gmail.com>
2020-07-11 14:23:06 +0200

    Dodanie uwag

--- src/com/inqoo/basics/HelloWorld.java
+++ src/com/inqoo/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Dzień dobry w deszczowy poranek, który jednak nie jest taki zły");
+        System.out.println("Dlatego bierzcie sie do nauki !");
     }
 }
