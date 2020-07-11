c98f0fcf3291b7bc4c0a386fd927fbad2a2348b1
Olaf Goj <olaf@goj.email>
2020-07-11 14:24:26 +0200

    sloneczny

--- src/com/inqoo/basics/HelloWorld.java
+++ src/com/inqoo/basics/HelloWorld.java
@@ -3,6 +3,6 @@ package com.inqoo.basics;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Dzień dobry w deszczowy poranek, który jednak nie jest taki zły");
+        System.out.println("Dzień dobry w słoneczny poranek, który jednak nie jest taki zły");
     }
 }
